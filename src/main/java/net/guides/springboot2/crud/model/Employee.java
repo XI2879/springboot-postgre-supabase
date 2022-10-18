056e8100d82af72f9f0b4dc5f7099247a739b9f0
XI2879 <91110151+XI2879@users.noreply.github.com>
2022-10-18 16:36:04 +0530

    Update Employee.java

--- src/main/java/net/guides/springboot2/crud/model/Employee.java
+++ src/main/java/net/guides/springboot2/crud/model/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "employees")
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "first_name", nullable = false)
